17291e528be3ff3001463b5ec14cd9449c316011
jandedeuraerder-howest <jan.de.deurwaerder@howest.be>
2020-04-23 08:10:27 +0200

    Add project files.

---
+++ PE2.LIB/Helper/ADRef.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
+
+namespace PE2.LIB.Helper
+{
+    public class ADRef
+    {
+        public const string LDAPLong = "LDAP://127.0.0.1:389/";
+        public const string LDAPShort = "LDAP://";
+        public const string ADDomainEmail = "@aitjdd.local";
+        public const string ADDomainNameShort = "aitjdd";
+        public const string ADDomainNameLong = "aitjdd.local";
+
+        public static bool DoesUserExits(string username)
+        {
+            PrincipalContext pc = new PrincipalContext(ContextType.Domain);
+            UserPrincipal up = UserPrincipal.FindByIdentity(pc, IdentityType.SamAccountName, username);
+            if (up == null)
+                return false;
+            else
+                return true;
+
+        }
+    }
+}
---
+++ PE2.WPF/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using PE2.LIB.Helper;
+
+namespace PE2.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public static void DoEvents()
+        {
+            Application.Current.Dispatcher.Invoke(DispatcherPriority.Background, new Action(delegate { }));
+        }
+
+        private void btnReadCSV_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
+
+            ofd.DefaultExt = ".csv";
+            ofd.Filter = "CSV|*.csv";
+            if(ofd.ShowDialog() == true)
+            {
+                string bestand = ofd.FileName;
+                tbKCSV.Text = System.IO.File.ReadAllText(bestand);
+            }
+
+
+        }
+
+        private void btnProcess_Click(object sender, RoutedEventArgs e)
+        {
+            tbKRapport.Text = "";
+            string inhoud = tbKCSV.Text;
+            string[] lijnen = inhoud.Split(new Char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            this.Cursor = Cursors.Wait;
+            foreach(string lijn in lijnen)
+            {
+                ProcessUser(lijn);
+                DoEvents();
+            }
+            this.Cursor = Cursors.Arrow;
+        }
+        private void ProcessUser(string lijn)
+        {
+            string[] waarden;
+            string[] groepen;
+
+            waarden = lijn.Split(';');
+            string username = waarden[0];
+            string firstname = waarden[1];
+            string lastname = waarden[2];
+            string expirationdate = waarden[3];
+            string paswoord = waarden[4];
+            string ou = waarden[5];
+            groepen = waarden[6].Split(',');
+
+            if(ADRef.DoesUserExits(username))
+            {
+                tbKRapport.Text += $"Updating {username} ... ";
+                // pas gebruiker aan
+
+                tbKRapport.Text += $" - {username} succesfully updated\n";  // uiteraard nog aanpassen
+            }
+            else
+            {
+                tbKRapport.Text += $"Adding {username} ... ";
+                // voeg gebruiker toe
+
+                tbKRapport.Text += $" - {username} succesfully added\n";  // uiteraard nog aanpassen
+            }
+
+        }
+    }
+}
